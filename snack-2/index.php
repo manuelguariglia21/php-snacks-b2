ff74bda3d9b6c9d880da20b7dd5bf719c393006a
Manuel Guariglia <manuelguariglia@gmail.com>
2021-12-26 21:54:39 +0100

    snack 2 started

--- snack-2/index.php
+++ snack-2/index.php
@@ -1,43 +1,5 @@
 <?php
-    $parking = $_GET['parking'];
-    $vote = $_GET['vote'];
-    $hotels = [
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
-    ];
+  
 ?>
 
 
@@ -47,16 +9,13 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Snack 9</title>
+  <title>Snack 2</title>
 </head>
 <body>
-  <h1>Snack 9</h1>
-    <?php
-      foreach($hotels as $hotel){
-        if( $hotel['vote'] > $vote ){
-          var_dump($hotel);
-        }
-      }
-    ?>
+  <h1>Snack 2</h1>
+  <form action=“server.php” method=“get”>
+        <input type=“text” placeholder=“Inserisci il tuo nome” name=“name”>
+        <button type=“submit” value=“Entra”>Entra</button>
+  </form>
 </body>
 </html>
