8386f7da744ef189fcc04c1bdaf672d75618807e
Manuel Guariglia <manuelguariglia@gmail.com>
2021-12-26 21:47:54 +0100

    first commit

---
+++ snack-1/index.php
@@ -0,0 +1,56 @@
+<?php
+
+$ads = [
+    [
+        'image_path' => 'https://conversionsciences.com/wp-content/uploads/2019/04/example-of-emotional-logical-appeal-on-persuasive-copy.jpg',
+        'link' => 'https://www.google.com',
+        'is_active' => true,
+    ],
+    [
+        'image_path' => 'https://previews.123rf.com/images/hstrongart/hstrongart1612/hstrongart161200121/68060446-anuncios-de-cepillos-de-dientes-el%C3%A9ctricos-modo-diferente-de-este-producto-con-blanco-modelo-de-diente.jpg',
+        'link' => 'https://www.facebook.com',
+        'is_active' => true,
+    ],
+    [
+        'image_path' => 'https://thumbs.dreamstime.com/z/back-to-school-sale-background-chalkboard-sale-percentages-marketing-poster-color-pencils-96751666.jpg',
+        'link' => 'https://codepen.io',
+        'is_active' => false,
+    ],
+    [
+        'image_path' => 'https://foodsecurityfoodjustice.files.wordpress.com/2016/11/beyond-the-omlette-1.jpg',
+        'link' => 'https://laravel.com',
+        'is_active' => false,
+    ],
+    [
+        'image_path' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRniLfGdFLxVOUoEwYC9WeRIrfZInB74_80IV8yMnANx3HTZYoQ',
+        'link' => 'https://www.php.net',
+        'is_active' => true,
+    ],
+
+];
+$activeAds = [];
+foreach($ads as $ad){
+  if($ad['is_active']){
+    array_push($activeAds,$ad);
+  }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Snack 1</title>
+</head>
+<body>
+  <h1>Snack 1</h1>
+    <?php
+      echo '<img src="';
+      echo $activeAds[rand(0, count($activeAds) - 1)]['image_path'];
+      echo '" alt="">';
+    ?>
+</body>
+</html>
---
+++ snack-2/index.php
@@ -0,0 +1,62 @@
+<?php
+    $parking = $_GET['parking'];
+    $vote = $_GET['vote'];
+    $hotels = [
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+    ];
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Snack 9</title>
+</head>
+<body>
+  <h1>Snack 9</h1>
+    <?php
+      foreach($hotels as $hotel){
+        if( $hotel['vote'] > $vote ){
+          var_dump($hotel);
+        }
+      }
+    ?>
+</body>
+</html>
